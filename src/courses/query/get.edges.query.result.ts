9f38eb1bbea6efcbba16bc324f1a9bb706bb6393
hoseok <hs1j333@gmail.com>
2025-08-18 11:56:18 +0900

    fix/course

--- src/courses/query/get.edges.query.result.ts
+++
@@ -1,9 +0,0 @@
-import { ValidateNested } from "class-validator";
-import { Type } from "class-transformer";
-import { CourseEdge } from "../dto";
-
-export class GetEdgesQueryResult {
-    @ValidateNested({ each: true })
-    @Type(() => CourseEdge)
-    edges: CourseEdge[];
-}
--- src/courses/query/index.ts
+++
@@ -1,5 +0,0 @@
-
-export * from "./get.edges.query.handler";
-export * from "./get.edges.query.result";
-export * from "./get.progress.query.handler";
-export * from "./get.progress.query.result";
